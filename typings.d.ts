615af9ca10c41f7d6f1b7b1e980570db864007f9
Max Milton <max@wearegenki.com>
2018-01-27 17:20:02 +1100

    Fix types for VS Code

--- typings.d.ts
+++ typings.d.ts
@@ -1,28 +1,7 @@
-// import _Vue, {
-//   VueConstructor,
-//   Component
-// } from 'vue';
-// import _Vuex from 'vuex';
-
 import _Vue from 'vue';
 import _Vuex from 'vuex';
 
 declare global {
-  export const VueGlobal: typeof _Vue;
-  // export const VueConstructor: VueConstructor;
-  // export const Component: Component;
-  export const Vuex: typeof _Vuex;
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $db: string;
-  }
-}
-
-// ----------------------------------------------------------------------------------
-
-declare module 'workerize-loader!*' {
-  const WebWorker: Worker;
-  export default WebWorker;
+  export const VueComponent: typeof _Vue;
+  export const VuexStore: typeof _Vuex.Store;
 }
